dbe9780d4fc3efd95a7e7439009e4febd0770bc4
Arunabh Sharma <117194178+imarunabh@users.noreply.github.com>
2023-09-30 21:13:37 +0530

    Add files via upload
    This project expresses about URL Rewriting concept

---
+++ eclipse-workspace/youtube1/src/main/java/myservlets/Servlet1.java
@@ -0,0 +1,66 @@
+package myservlets;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet1")
+public class Servlet1 extends HttpServlet {
+	protected void processRequest(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+		response.setContentType("text/html;charset=UTF-8");
+		try(PrintWriter out= response.getWriter()){
+			out.println("<!DOCTYPE html>");
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<title>Servlet servlet</title>");
+			out.println("</head>");
+			out.println("<body>");
+			//name fetch
+			String name = request.getParameter("user_name");
+			out.println("<h1>Your name is :"+name+"</h1>");
+			out.println("<a href='servlet2?user="+name+"'>Go to second servlet</a>"); 
+			
+			
+			
+			
+			
+			out.println("</body>");
+			out.println("</html>");
+			
+			
+		}
+		
+	}
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request,response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+	
+	@Override
+	public String getServletInfo() {
+		return "Short description";
+	}
+
+}
---
+++ eclipse-workspace/youtube1/src/main/java/myservlets/Servlet2.java
@@ -0,0 +1,64 @@
+package myservlets;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+/**
+ * Servlet implementation class Servlet1
+ */
+@WebServlet("/Servlet2")
+public class Servlet2 extends HttpServlet {
+	protected void processRequest(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+		response.setContentType("text/html;charset=UTF-8");
+		try(PrintWriter out= response.getWriter()){
+			out.println("<!DOCTYPE html>");
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<title>Servlet servlet</title>");
+			out.println("</head>");
+			out.println("<body>");
+			//name fetch
+			String name = request.getParameter("user");
+			out.println("<h1 style='color:red;'>welcome back"+name+"</h1>");
+			
+			
+			
+			
+			out.println("</body>");
+			out.println("</html>");
+			
+			
+		}
+		
+	}
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		processRequest(request,response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+	
+	@Override
+	public String getServletInfo() {
+		return "Short description";
+	}
+
+}
